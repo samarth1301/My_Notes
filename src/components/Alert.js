3fcb3ebc9fb47ad3d73149c14bd6bda53164a3b2
samarth1301 <samarthsingh890.ss@gmail.com>
2021-12-03 13:16:54 +0530

    changed alert color

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Alert = (props) => {
     return (
-        <div className={`w-max max-w-1/2 bg-${props.color}-400 fixed top-7 text-center right-14 md:right-24 md:top-12 rounded-lg px-4 py-2 font-semibold `}>
+        <div className={`w-max max-w-1/2 bg-red-400 fixed top-7 text-center right-14 md:right-24 md:top-12 rounded-lg px-4 py-2 font-semibold `}>
             {props.message}
         </div>
     )
